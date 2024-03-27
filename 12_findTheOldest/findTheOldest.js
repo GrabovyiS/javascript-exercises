8a46cc168076ea0d2d6059983088b321b9bce65c
GrabovyiS <graboviys@yandex.ru>
2024-03-28 00:51:15 +0300

    finish foundation exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  let oldestAge = 0;
+  let oldest;
 
+  people.forEach(person => {
+    const currentYear = new Date().getFullYear();
+    let age = person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : new Date().getFullYear() - person.yearOfBirth;
+    if (age > oldestAge) {
+      oldest = person;
+      oldestAge = age;
+    }
+  });
+
+  return oldest;
 };
 
 // Do not edit below this line
